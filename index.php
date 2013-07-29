ea25cab62a1e316f3cd4b32e62944d0dbf230107
bwinkers <bwinkers@gmail.com>
2013-07-29 17:41:28 -0500

    Barebones index file

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * Copyright 2013 - Brian Winkers
+ */
+
+// Set the full path to the docroot
+define('DOCROOT', realpath(dirname(__FILE__)).DIRECTORY_SEPARATOR);
+
+// echo DOCROOT;
+
+// var_export($_SERVER);
+
+?>
