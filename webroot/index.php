b5ef6908e589fbdecf7561e5ecabf075b2f1f536
bwinkers <bwinkers@gmail.com>
2013-08-28 21:16:23 -0500

    Basic Front Controller implemented for AppFog

---
+++ webroot/index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * This is the ActiveRules index.php file.
+ * This is the Front Controller.
+ * The web server environment needs to route all request to this file
+ * 
+ * @package ActiveRules
+ * @subpackage Bootstrap
+ */
+
+/**
+ * Define the ActiveRules version number
+ */
+define('AR_VERSION', '7.1');
+
+
+
+echo AR_VERSION;
